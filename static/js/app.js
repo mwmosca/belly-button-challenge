2fcdea948785698f3aaf47f90f2a312e9c497d2d
mwmosca <mwmosca93@gmail.com>
2024-04-23 17:51:11 -0400

    create bar chart on dropdown selection

--- static/js/app.js
+++ static/js/app.js
@@ -1,9 +1,10 @@
 // Get the data endpoint
 const url = 'https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json';
-
-// Load the data to be used locally
+// Create a container to store the data locally
 const dataset = {};
+
 d3.json(url).then(function(data) {
+    // Load the data to be used locally
     for (i = 0; i < data['names'].length; i++) {
         dataset[data['names'][i]] = data['samples'][i];
     }
@@ -13,7 +14,7 @@ d3.json(url).then(function(data) {
 
 // Initializes the page with default plots
 function init(dataset, testSubjectId) { 
-    // Add data to the dropdown
+    // Add test subject IDs to the dropdown
     let dropDown = d3.select('select');
     for (const key in dataset) {
         dropDown.append('option').text(key);
@@ -26,6 +27,7 @@ function init(dataset, testSubjectId) {
 // Updates plots when a new test subject is selected from the dropdown
 function optionChanged() {
     let testSubjectId = d3.select('#selDataset').property('value');
+    createBar(dataset[testSubjectId]);
 }
 
 // Create a trace for a bar chart to display the top 10 OTUs found in the selected individual
