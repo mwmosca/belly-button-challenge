f00c8dc116b203cfd40fcce316c292c071f380c1
mwmosca <mwmosca93@gmail.com>
2024-04-25 19:18:34 -0400

    display metadata

--- static/js/app.js
+++ static/js/app.js
@@ -8,14 +8,13 @@ d3.json(url).then(function(data) {
     for (i = 0; i < data['names'].length; i++) {
         dataset[data['names'][i]] = {
             samples: data['samples'][i],
-            metadata: 
-`ID: ${data['metadata'][i]['id']}
-ETHNICITY: ${data['metadata'][i]['ethnicity']}
-GENDER: ${data['metadata'][i]['gender']}
-AGE: ${data['metadata'][i]['age']}
-LOCATION: ${data['metadata'][i]['location']}
-BBTYPE: ${data['metadata'][i]['bbtype']}
-WFREQ: ${data['metadata'][i]['wfreq']}`
+            metadata: ` ID: ${data['metadata'][i]['id']}
+                        ETHNICITY: ${data['metadata'][i]['ethnicity']}
+                        GENDER: ${data['metadata'][i]['gender']}
+                        AGE: ${data['metadata'][i]['age']}
+                        LOCATION: ${data['metadata'][i]['location']}
+                        BBTYPE: ${data['metadata'][i]['bbtype']}
+                        WFREQ: ${data['metadata'][i]['wfreq']}`
         };
     }
 
@@ -26,21 +25,23 @@ WFREQ: ${data['metadata'][i]['wfreq']}`
 // Initializes the page with default plots
 function init(dataset, testSubjectId) { 
     // Add test subject IDs to the dropdown
-    let dropDown = d3.select('select');
+    let dropDown = d3.select('#selDataset');
     for (const key in dataset) {
         dropDown.append('option').text(key);
     }
 
-    // Set up plots
+    // Set up plots and demographic info
     createBar(dataset[testSubjectId]['samples']);
     createBubble(dataset[testSubjectId]['samples']);
+    d3.select('#sample-metadata').append('card-text').text(dataset[testSubjectId]['metadata']);
 }
 
-// Updates plots when a new test subject is selected from the dropdown
+// Updates plots and demographic info when a new test subject is selected from the dropdown
 function optionChanged() {
     let testSubjectId = d3.select('#selDataset').property('value');
     createBar(dataset[testSubjectId]['samples']);
     createBubble(dataset[testSubjectId]['samples']);
+    d3.select('card-text').text(dataset[testSubjectId]['metadata']);
 }
 
 // Create a bar chart to display the top 10 OTUs found in the selected individual
@@ -94,8 +95,4 @@ function createBubble(testSubjectData) {
         }
     };
     Plotly.newPlot('bubble', plots, layout);
-}
-
-function createMetadata(testSubjectData) {
-
 }
